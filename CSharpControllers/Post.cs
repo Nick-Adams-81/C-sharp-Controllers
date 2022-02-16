98e0864727b8aeb050abefc4fb7f1bb2a7adda03
Nick Adams <nick.pappas.adams@gmail.com>
2022-02-16 14:27:56 -0600

    added post method

---
+++ CSharpControllers/Post.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CSharpControllers
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public int UserId { get; set; }
+
+    }
+}
--- CSharpControllers/Program.cs
+++ CSharpControllers/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace CSharpControllers
 {
@@ -7,11 +9,28 @@ namespace CSharpControllers
     {
         static void Main(string[] args)
         {
-            using (var client = new HttpClient())
+            //using (var client = new HttpClient())
+            //{
+            //    var endpoint = new Uri("https://jsonplaceholder.typicode.com/posts");
+            //    var result = client.GetAsync(endpoint).Result;
+            //    var json = result.Content.ReadAsStringAsync().Result;
+            //}
+
+
+            using(var client2 = new HttpClient())
             {
                 var endpoint = new Uri("https://jsonplaceholder.typicode.com/posts");
-                var result = client.GetAsync(endpoint).Result;
-                var json = result.Content.ReadAsStringAsync().Result;
+                var newPost = new Post()
+                {
+                    Title = "Test post",
+                    Body = "Hello world!",
+                    UserId = 55
+                };
+                var newPostJson = JsonConvert.SerializeObject(newPost);
+                var payload = new StringContent(newPostJson, Encoding.UTF8, "application/json");
+                var result = client2.PostAsync(endpoint, payload).Result.Content.ReadAsStringAsync().Result;
+
+
             }
         }
     }
