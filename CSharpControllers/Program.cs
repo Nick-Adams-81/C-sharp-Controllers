c42abe1ed59877717cc5ff3ac8512011466e5f7b
Nick Adams <nick.pappas.adams@gmail.com>
2022-02-16 13:56:04 -0600

    first commit

---
+++ CSharpControllers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpControllers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
